7324fed9525c208d717efde1616ece8623aefd34
sudeshiuthpala <sudeshiuthpala98@gmail.com>
2021-07-22 01:20:28 +0530

    professional Dashboard Completed

--- fifthApp/src/app/app.module.ts
+++ fifthApp/src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from '@angular/core';
+import { NgModule,CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicModule, IonicRouteStrategy, IonProgressBar } from '@ionic/angular';
 import { IonicStorageModule} from '@ionic/storage-angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import { HttpClientModule } from '@angular/common/http';
-import { Ng2GoogleChartsModule } from 'ng2-google-charts';
+import { GoogleChartComponent, Ng2GoogleChartsModule } from 'ng2-google-charts';
 //import { Chart } from 'chart.js';
 
 @NgModule({
@@ -18,5 +18,6 @@ import { Ng2GoogleChartsModule } from 'ng2-google-charts';
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,IonicStorageModule.forRoot(),HttpClientModule, Ng2GoogleChartsModule ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
